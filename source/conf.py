c1900571a14167f458d1c332d9fff528de186d35
ibreakout <48092485+ibreakout@users.noreply.github.com>
2019-09-22 19:08:57 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -24,6 +24,7 @@ author = 'test'
 # The full version, including alpha/beta/rc tags
 release = '1.0'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
